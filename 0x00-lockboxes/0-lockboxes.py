8c0750ee315229ecbe7617d142bb0b0bebdd39b6
Anna78990 <takasuanna1990@gmail.com>
2022-12-12 10:29:46 +0000

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-import sys
+""" Lockboxes """
+
 
 def canUnlockAll(boxes):
-    check = []
+""" determines if all the boxes can be opened """
+    check = [0]
 
-    if len(boxes) == 0:
-         return True
-    for i in range(0, len(boxes)):
-         check.append(0)
-    recursiveCheck(boxes, 0, check)
-    if 0 in check:
-        return False
-    else:
+    for i in check:
+        for j in boxes[i]:
+            if j not in check:
+                check.append(j)
+
+    if len(check) == (len(boxes) - 1):
         return True
-
-def recursiveCheck(*argv):
-    cur = argv[1]
-    if len(argv[0][cur]) > 0:
-        for i in argv[0][cur]:
-            if argv[2][i] == 1:
-                continue
-            else:
-                argv[2][i] == 1
-                recursiveCheck(argv[0], i, argv[2])
+    else:
+        return False
