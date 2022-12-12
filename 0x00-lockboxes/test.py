05261f01ed1e4b52684b7ac9f1764b138f2356d4
Anna78990 <takasuanna1990@gmail.com>
2022-12-12 14:48:41 +0000

    if

--- 0x00-lockboxes/test.py
+++ 0x00-lockboxes/test.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-list = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-t = tuple(list)
-tup = ()
+a = [1, 2, 3]
+b = [1, 2]
 
+a = ''.join(map(str, a))
+b = ''.join(map(str, b))
 
-def func(*args):
-    print(args[0][0])
-    print(args[1])
-    print(len(args))
-
-func([[1, 2], 2], [3, 1])
-
+print(a)
+print(b)
+print(b in a)
