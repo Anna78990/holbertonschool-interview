29f269dac6347a35be48340353c6efe22e272cea
Anna78990 <takasuanna1990@gmail.com>
2022-12-12 14:28:23 +0000

    segf

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,7 +8,7 @@ def canUnlockAll(boxes):
     a = 0
     indexes = []
     for n in boxes:
-        indexes.append(boxes)
+        indexes.append(a)
         a += 1
 
     for i in check:
@@ -17,7 +17,10 @@ def canUnlockAll(boxes):
                 if j not in check:
                     check.append(j)
         except IndexError as e:
-            return False
+            if check[-2] == indexes[-1]:
+                return True
+            else:
+                return False
     if len(check) >= len(indexes):
         return True
     else:
