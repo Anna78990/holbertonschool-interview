ea16200cfe5327ed2f404bcbcca7ad8699cfb9f3
Anna78990 <takasuanna1990@gmail.com>
2023-03-31 17:22:42 +0000

    0-sort

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,47 +1,59 @@
 #include "binary_trees.h"
 
-int find_idx(int *array, size_t size)
-{
-	if ((int)size % 2 == 1)
-		return ((int)size / 2);
-	else if ((int)size % 2 == 0)
-		return (((int)size / 2) - 1);
 
+/**
+ * create_node - create new node
+ * @n: n of node
+ * @parent: parent of a node
+ *
+ * Return: pointer to new node
+ **/
+avl_t *create_node(int n, avl_t *parent)
+{
+	avl_t *new;
+
+	new = (avl_t *)malloc(sizeof(avl_t));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->parent = parent;
+	return (new);
 }
 
-int find_height(size_t size)
+/**
+ * binary_recursion - create recursively the node child
+ * @parent: pointer to the parent
+ * @array: array to be tree
+ * @left: the index to be the left part of the tree
+ * @right: the index to be the right part of the tree
+ *
+ * Return: modified tree
+ */
+avl_t *binary_recursion(avl_t *parent, int *array, int left, int right)
 {
-	int ctr = 0;
+	avl_t *new;
+	int mid;
 
-	while ((int)size /= 2)
-		ctr++; 
+	if (left > right)
+		return (NULL);
+	mid = (left + right) / 2;
+	new = create_node(array[mid], parent);
+	if (!new)
+		return (NULL);
+	new->left = binary_recursion(new, array, left, mid - 1);
+	new->right = binary_recursion(new, array, mid + 1, right);
+	return (new);
 }
 
-
-
+/**
+ * sorted_array_to_avl - create a binary tree by given array
+ * @array: array
+ * @size: size of array
+ *
+ * Return: pointer to root of tree
+ **/
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *node, *swp;
-	int mid, i;
-
-	if (array == NULL)
-		return (NULL);
-	mid = find_idx(array, size);
-	node = (avl_t*)malloc(sizeof(avl_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = array[mid];
-	node->parent = NULL;
-
-	for (i = 0; array[i]; i++)
-	{
-		while (node != NULL)
-		{
-			if (array[i] < node->n)
-				node = node->right;
-			else
-				node = node->left;
-		}
-
-	}
+	return (binary_recursion(NULL, array, 0, (int)size - 1));
 }
