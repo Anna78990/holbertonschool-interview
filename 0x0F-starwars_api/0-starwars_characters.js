9e33294c0c9cdb9058f5de4c0f9baff858b0ee6e
Anna78990 <takasuanna1990@gmail.com>
2023-04-18 13:32:27 +0000

    semi

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,13 +4,13 @@ const request = require('request');
 
 function characterLoop (character, i) {
   if (i === character.length) return;
-    request(character[i], function (err, res, body) {
-      if (err) console.log(err);
-      else {
-        console.log(JSON.parse(body).name);
-        characterLoop(character, ++i);
-      }
-    });
+  request(character[i], function (err, res, body) {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.parse(body).name);
+      characterLoop(character, ++i);
+    }
+  });
 }
 
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
