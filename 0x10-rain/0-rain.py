96a255d2da43b951b6798b5cae556d7c996afd69
Anna78990 <takasuanna1990@gmail.com>
2023-06-01 09:43:15 +0000

    docc

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""rain module"""
 
 
 def rain(walls):
     """
-    Given a list of non-negative integers representing
-    the heights of walls with unit width 1
-    as if viewing the cross-section of a relief map,
-    calculate how many square units of water will be retained after it rains.
+    calculate how much water can be trapped
+    Args:
+        walls: list of height values
+    Returns:
+        amount of water able to be trapped
     """
     if len(walls) < 3:
         return 0
