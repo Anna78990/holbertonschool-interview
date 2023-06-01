b992100e97b5bea6ad78078b1490f6efcfaf73b1
Anna78990 <takasuanna1990@gmail.com>
2023-06-01 08:15:09 +0000

    if main

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -53,20 +53,20 @@ def print_solution(board):
     print(solution)
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
 
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-board = [[0] * N for _ in range(N)]
-
-solve_nqueens(board, 0)
+    board = [[0] * N for _ in range(N)]
+    solve_nqueens(board, 0)
