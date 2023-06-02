9e157f8af9ead4304fa1e7742200b76a04f9d96c
Anna78990 <takasuanna1990@gmail.com>
2023-06-02 07:46:25 +0000

    ifname

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -18,32 +18,33 @@ def print_stats(file_size, status_codes):
             print("{}: {}".format(code, status_codes[code]))
 
 
-line_count = 0
-file_size = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+if __name__ == '__main__':
+    line_count = 0
+    file_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        split_line = line.split()
-        if len(split_line) >= 7:
-            file_size += int(split_line[-1])
-            status_code = split_line[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            split_line = line.split()
+            if len(split_line) >= 7:
+                file_size += int(split_line[-1])
+                status_code = split_line[-2]
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
 
-        if line_count % 10 == 0:
-            print_stats(file_size, status_codes)
+            if line_count % 10 == 0:
+                print_stats(file_size, status_codes)
 
-except KeyboardInterrupt:
-    print_stats(file_size, status_codes)
-    raise
+    except KeyboardInterrupt:
+        print_stats(file_size, status_codes)
+        raise
