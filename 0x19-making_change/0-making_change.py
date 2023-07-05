a0a93ec442c5d48bee9c1f19bec53d382a86d123
Anna78990 <takasuanna1990@gmail.com>
2023-07-05 04:36:49 +0000

    exception

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
 determine the fewest number of coins needed
-to meet a given amount total
+to meet a given total total
 """
 
 
-def makeChange(coins, amount):
-    dp = [float('inf')] * (amount + 1)
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, amount + 1):
+    for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
                 dp[i] = min(dp[i], 1 + dp[i - coin])
 
-    if dp[amount] == float('inf'):
+    if dp[total] == float('inf'):
         return -1
     else:
-        return dp[amount]
+        return dp[total]
