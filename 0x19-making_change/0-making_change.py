4e20d3d7cdea85140a29e56f72d980e9ecd9f9dd
Anna78990 <takasuanna1990@gmail.com>
2023-07-06 13:14:53 +0000

    0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,11 +12,11 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
+    for i in range(0, total + 1):
         for coin in coins:
             if coin > i:
                 break
-            if coin <= i:
+            if dp[i - coin] != -1:
                 dp[i] = min(dp[i], 1 + dp[i - coin])
 
     if dp[total] == float('inf'):
