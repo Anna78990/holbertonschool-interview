b22578c818d4b1c3a49325feb5f9f6862f6b36b9
Anna78990 <takasuanna1990@gmail.com>
2023-07-17 04:56:24 +0000

    -

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
+
+/**
+ * get_max - Returns the maximum element from an array
+ * @array: The array
+ * @size: Number of elements in @array
+ * Return: The maximum element
+ */
+int get_max(int *array, size_t size)
+{
+	int max = array[0];
+	size_t i;
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * count_sort - Performs counting sort on an array based on a significant digit
+ * @arr: The array
+ * @n: Number of elements in array
+ * @exp: The significant digit to consider
+ */
+void count_sort(int arr[], int n, int exp)
+{
+	int output[n];
+	int i, count[10] = {0};
+
+	for (i = 0; i < n; i++)
+		count[(arr[i] / exp) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+	for (i = 0; i < n; i++)
+		arr[i] = output[i];
+}
+
+
+/**
+ * radix_sort - Sorts an array of integers in ascending order using the
+ *			  LSD (Least Significant Digit) radix sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max = get_max(array, size);
+	int exp;
+
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		count_sort(array, size, exp);
+		print_array(array, size);
+	}
+}
---
+++ 0x1B-radix_sort/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+
+	i = 20;
+	printf("20 / 100 = %d\n", (i / 100) % 2);
+	return 1;
+}
