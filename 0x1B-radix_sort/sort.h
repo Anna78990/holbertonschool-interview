16e95c713da6fed332991af891f15d39965b3ea5
Anna78990 <takasuanna1990@gmail.com>
2023-07-19 09:09:57 +0000

    modi hfile

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 
 void print_array(const int *array, size_t size);
+int get_max(int *array, size_t size);
+void count_sort(int arr[], int n, int exp);
 void radix_sort(int *array, size_t size);
 
 #endif
