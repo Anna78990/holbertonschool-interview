0ca2745b7aa608ba2b9f499ff30cdc9dde2db0b3
Anna78990 <takasuanna1990@gmail.com>
2023-12-04 07:00:41 +0000

    change dir name

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function characterLoop (character, i) {
+  if (i === character.length) return;
+  request(character[i], function (err, res, body) {
+    if (err) console.log(err);
+    else {
+      console.log(JSON.parse(body).name);
+      characterLoop(character, ++i);
+    }
+  });
+}
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`,
+  function (err, res, body) {
+    if (err) console.log(err);
+    else {
+      const charters = JSON.parse(body).characters;
+      characterLoop(charters, 0);
+    }
+  });
